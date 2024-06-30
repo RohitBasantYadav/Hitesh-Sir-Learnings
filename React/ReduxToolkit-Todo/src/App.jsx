2d881e985f9ac16ee0261f4844b7c0c6e09d5b60
RohitBasantYadav <yadav.rohit.basant@gmail.com>
2024-06-30 23:18:07 +0530

    Redux practice done

---
+++ React/ReduxToolkit-Todo/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./components/AddTodo"
+import Todo from "./components/Todo"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">This is Redux Toolkit Examples</h1>
+      <AddTodo/>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
