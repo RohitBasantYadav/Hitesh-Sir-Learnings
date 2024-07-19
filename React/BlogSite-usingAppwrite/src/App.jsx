a7169566204c26c2ef5fb1e0a04e9454d77ab4a5
RohitBasantYadav <yadav.rohit.basant@gmail.com>
2024-07-19 16:12:56 +0530

    Working on Pages

--- React/BlogSite-usingAppwrite/src/App.jsx
+++ React/BlogSite-usingAppwrite/src/App.jsx
@@ -1,10 +1,45 @@
-import './App.css'
+import { useEffect, useState } from "react"
+import authService from "./appwrite/auth";
+import { logout, login } from "./features/authSlice";
+import { useDispatch } from "react-redux";
+import { Spinner } from '@chakra-ui/react'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import {Outlet} from "react-router-dom"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
+  }, [])
+
+  if (loading) {
+    return <Spinner
+      thickness='4px'
+      speed='0.65s'
+      emptyColor='gray.200'
+      color='blue.500'
+      size='xl'
+    />
+  }
   return (
     <>
-      <h1>This is Heading</h1>
+    <Navbar/>
+      <Outlet/>
+    <Footer/>
     </>
   )
 }
--- React/BlogSite-usingAppwrite/src/appwrite/auth.js
+++ React/BlogSite-usingAppwrite/src/appwrite/auth.js
@@ -49,7 +49,6 @@ export class AuthService{
         catch(err){
             return err;
         }
-        return null;
     }
 
 }
